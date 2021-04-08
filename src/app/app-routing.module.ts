fca9d2879d491b96227a6f3b978948a3e0f976ba
Fabrizio Manna <mannafabrizio83@gmail.com>
2021-04-08 11:00:40 +0200

    Revert "Initial commit"
    This reverts commit 8eaaefc391b070f294fd272d0dbcbfc4b071250e.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: '', loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule)
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/modules/login/components/leftbox/leftbox.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-leftbox',
-  templateUrl: './leftbox.component.html',
-  styleUrls: ['./leftbox.component.scss']
-})
-export class LeftboxComponent { }
