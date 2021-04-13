241ee639d4e373a91a94c94e6146ac9cad95a37c
Fabrizio Manna <mannafabrizio83@gmail.com>
2021-04-13 17:57:32 +0200

    Mofidy login.component.ts
    Insert Router for change on load page if user is loggedin

--- src/app/modules/login/pages/login/login.component.ts
+++ src/app/modules/login/pages/login/login.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   public spinner = false;
+
+  constructor(
+    private route: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.changeSpinner();
+    const token: (string | null) = localStorage?.getItem('token') || null;
+    if (token == null) {
+      localStorage.clear();
+      this.changeSpinner();
+    }
+    else {
+      this.changeSpinner();
+      this.route.navigate(['/', 'dashboard']);
+    }
+  }
 
   public changeSpinner(): void {
     this.spinner = !this.spinner;
