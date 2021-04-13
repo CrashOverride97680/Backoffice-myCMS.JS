4601ae098c162db059f263a99d2177cc37795e9b
Fabrizio Manna <mannafabrizio83@gmail.com>
2021-04-13 17:53:15 +0200

    Create module for refactoring dashboard
    Create this module for create a scalable app

--- src/app/modules/dashboard/dashboard-routing.module.ts
+++ src/app/modules/dashboard/dashboard-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AdminGuard } from '../../core/guards/admin-guard/admin-guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent,
+    canActivate: [AdminGuard]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
