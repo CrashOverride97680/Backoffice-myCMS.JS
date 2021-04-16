931701f4b86e043ba2b2167635d4cdba28d99be8
Fabrizio Manna <mannafabrizio83@gmail.com>
2021-04-16 14:25:31 +0200

    Modify admin-guard
    Create state for aside change on reload and on change hide or show aside

--- src/app/core/guards/admin-guard/admin.guard.ts
+++ src/app/core/guards/admin-guard/admin.guard.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate } from '@angular/router';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { AsideService } from '../../static/components/aside/services/aside.service';
 @Injectable({
   providedIn: 'root'
 })
 export class AdminGuard implements CanActivate {
 
   constructor(
-    public router: Router
+    public router: Router,
+    private aside: AsideService
   ) { }
 
   canActivate(): any {
@@ -19,13 +21,16 @@ export class AdminGuard implements CanActivate {
         const auth: boolean = jwtHelper.decodeToken(token)?.auth || false;
         const admin: boolean = jwtHelper.decodeToken(token)?.admin || false;
         if (admin && auth) {
+          this.aside.changeAsideData({view: true});
           return true;
         }
       }
     }
     catch (e) {
       localStorage.clear();
-      this.router.navigate(['/']);
+      this.router.navigate(['/']).then(() => {
+        this.aside.changeAsideData({view: false});
+      });;
     }
   }
 }
