ec61cd23167f6b6c14bfcc300fcb213dc00f9882
Fabrizio Manna <mannafabrizio83@gmail.com>
2021-04-16 14:40:23 +0200

    Modify dashboard-routing.module.ts
    Refactory name guard (Admin Guard) path and name

--- src/app/modules/dashboard/dashboard-routing.module.ts
+++ src/app/modules/dashboard/dashboard-routing.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { AdminGuard } from '../../core/guards/admin-guard/admin-guard';
-
+import { AdminGuard } from '../../core/guards/admin-guard/admin.guard';
 const routes: Routes = [
   {
     path: '',
