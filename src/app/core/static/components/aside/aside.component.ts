13d572c3a8f497e265508c9246cf3e39c20a56df
Fabrizio Manna <mannafabrizio83@gmail.com>
2021-04-16 14:38:42 +0200

    Modify aside.component.ts
    Remove all whitespaces

--- src/app/core/static/components/aside/aside.component.ts
+++ src/app/core/static/components/aside/aside.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-aside',
   templateUrl: './aside.component.html',
   styleUrls: ['./aside.component.scss']
 })
-export class AsideComponent {}
+export class AsideComponent { }
