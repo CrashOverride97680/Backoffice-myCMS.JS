1a63a3a4d78820113118ce17bf6295302c1b50bc
Fabrizio Manna <mannafabrizio83@gmail.com>
2021-04-16 14:42:03 +0200

    Modify login-routing.module.ts
    Reafctory import guard login

--- src/app/modules/login/login-routing.module.ts
+++ src/app/modules/login/login-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
-import { LoginGuard } from '../../core/guards/login-guard/login-guard.guard';
+import { LoginGuard } from '../../core/guards/login-guard/login.guard';
 
 const routes: Routes = [
   {
