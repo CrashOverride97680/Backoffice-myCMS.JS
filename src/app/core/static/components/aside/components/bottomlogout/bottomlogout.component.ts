6d09d1d73fbea1cab0dcddb176b66f51c7d9ef48
Fabrizio Manna <mannafabrizio83@gmail.com>
2021-04-19 12:28:53 +0200

    Modify angular.json
     Fix

---
+++ src/app/core/static/components/aside/components/bottomlogout/bottomlogout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bottomlogout',
+  templateUrl: './bottomlogout.component.html',
+  styleUrls: ['./bottomlogout.component.scss']
+})
+export class BottomlogoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/core/static/components/aside/components/dashlink/dashlink.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashlink',
+  templateUrl: './dashlink.component.html',
+  styleUrls: ['./dashlink.component.scss']
+})
+export class DashlinkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
