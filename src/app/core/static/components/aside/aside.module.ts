4a40419be7f2afee20d5cb63c100eac6f6487fb2
Fabrizio Manna <mannafabrizio83@gmail.com>
2021-04-19 12:37:54 +0200

    Modify aside.module.ts
    Update components list inao aside module

--- src/app/core/static/components/aside/aside.module.ts
+++ src/app/core/static/components/aside/aside.module.ts
@@ -3,14 +3,22 @@ import { CommonModule } from '@angular/common';
 import { AsideComponent } from './aside.component';
 import { DividerComponent } from './components/divider/divider.component';
 import { AsideLogoComponent } from './components/aside-logo/aside-logo.component';
+import { DashlinkComponent } from './components/dashlink/dashlink.component';
+import { RouterModule } from '@angular/router';
+import { NavgroupComponent } from './components/navgroup/navgroup.component';
+import { BottomlogoutComponent } from './components/bottomlogout/bottomlogout.component';
 @NgModule({
   declarations: [
     AsideComponent,
     DividerComponent,
-    AsideLogoComponent
+    AsideLogoComponent,
+    DashlinkComponent,
+    NavgroupComponent,
+    BottomlogoutComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     AsideComponent
