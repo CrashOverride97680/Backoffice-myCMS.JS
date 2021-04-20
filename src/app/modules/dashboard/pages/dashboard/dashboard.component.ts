6e646faefe5117dc2d706bb95dc1f67a0a5bf879
Fabrizio Manna <mannafabrizio83@gmail.com>
2021-04-20 13:52:13 +0200

    Modify dashboard.component.ts
    Remove bad practise for best with rxjs

--- src/app/modules/dashboard/pages/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/pages/dashboard/dashboard.component.ts
@@ -1,36 +1,38 @@
 import { Component } from '@angular/core';
-
+import { HttpService } from '../../../../core/http/http.service';
+import { forkJoin } from 'rxjs';
+import {HttpPostInterceptor} from '../../../../core/interceptors/http.interceptor';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+  public numPosts = 0;
+  public numMail = 0;
+  public numChat = 0;
+  public numPay = 0;
+  public view = true;
+  public posts: HttpPostInterceptor[] = [];
   constructor(
-    private api: ApiService
+    private api: HttpService
   ) {
-    const token = localStorage.getItem('token');
-    Promise
-      .all([
-        this.api.getNumPosts(token),
-        this.api.getNumMailSub(token),
-        this.api.getNumChat(token),
-        this.api.getNumPay(token),
-        this.api.getPostsByMaxNumber(token, 5)
-      ])
-      .then(value => {
-        this.postsData = value[0];
-        this.mailsubData = value[1];
-        this.chatData = value[2];
-        this.paymentsData = value[3];
-        this.postsorig = value[4];
-        this.posts = this.postsorig.map(el => {
-          let data = el;
-          data.updated = data.updated.split("T")[0];
-          return data;
-        });
-        console.log("POSTS:", this.posts);
-      });
+    const token: string = localStorage.getItem('token') || '';
+    const data = { token, max: 5 };
+    forkJoin([
+      this.api.getNumPosts(token),
+      this.api.getNumMailSub(token),
+      this.api.getNumChat(token),
+      this.api.getNumPay(token),
+      this.api.getPostsByMaxNumber(data)
+    ]).subscribe(res => {
+      this.numPosts = res[0].count;
+      this.numMail = res[1].count;
+      this.numChat = res[2].count;
+      this.numPay = res[3].count;
+      this.posts = res[4];
+      this.view = false;
+    });
   }
 }
 
