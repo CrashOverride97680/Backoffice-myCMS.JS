9c27dd02926188c5f0cae7dcc377a0ff4767bf8d
Fabrizio Manna <mannafabrizio83@gmail.com>
2021-04-29 09:18:55 +0200

    modify posts.component.ts
    modify posts

--- src/app/modules/posts/pages/posts/posts.component.ts
+++ src/app/modules/posts/pages/posts/posts.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { SettingsPrefixInterceptor } from '../../../../core/interceptors/ui-prefix.interceptor';
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.scss']
 })
-export class PostsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class PostsComponent {
+  public settings = {
+    hideSubHeader: false,
+    actions: false,
+    attr: {
+      class: 'table'
+    },
+    columns: {
+      _id: {
+        title: 'ID',
+        show: false
+      },
+      type: {
+        title: 'TYPE'
+      },
+      title: {
+        title: 'TITLE'
+      },
+      visible: {
+        title: 'VISIBLE',
+        filter: false
+      },
+      updated: {
+        title: 'DD/MM/YYYY'
+      },
+      actions: {
+        title: 'ACTIONS',
+        type: 'html',
+        filter: false,
+        sort: false,
+        valuePrepareFunction: (row: SettingsPrefixInterceptor) => {
+          return `<span><a href="/modifyPosts/${row._id}" title="modify ${row.title}" target="_blank">Modify</a> / <a href="/deletePosts/${row._id}" title="delete ${row._id}">Delete</a></span>`
+        }
+      }
+    }
+  };
 }
