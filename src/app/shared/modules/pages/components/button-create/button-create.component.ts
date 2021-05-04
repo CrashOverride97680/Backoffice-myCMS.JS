a6e216fdf40c6e3b76c2479c4f123688d936bed0
Fabrizio Manna <mannafabrizio83@gmail.com>
2021-05-04 14:09:33 +0200

    Create button-create.component.ts
    Create component stateless

--- src/app/shared/modules/pages/components/button-create/button-create.component.ts
+++ src/app/shared/modules/pages/components/button-create/button-create.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
-  selector: 'app-button-create',
-  templateUrl: './button-create.component.html',
+  selector: 'pages-button-create',
+  template: `
+    <div class="btn-adds" [ngClass]="class!">
+      <a [routerLink]="router" class="btn_add_internal">
+        <ng-content></ng-content>
+      </a>
+    </div>
+  `,
   styleUrls: ['./button-create.component.scss']
 })
-export class ButtonCreateComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ButtonCreateComponent {
+  @Input() class = '';
+  @Input() router = [''];
+  @Input() title = '';
 }
