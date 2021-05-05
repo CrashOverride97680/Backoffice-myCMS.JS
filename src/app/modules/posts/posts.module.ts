8bbe721b773ef95eaccf4ff2eaa733bcb5210296
Fabrizio Manna <mannafabrizio83@gmail.com>
2021-05-05 15:54:45 +0200

    Modify posts.module.ts
    Importing anfular editor wysiwig

--- src/app/modules/posts/posts.module.ts
+++ src/app/modules/posts/posts.module.ts
@@ -5,13 +5,15 @@ import { PostsRoutingModule } from './posts-routing.module';
 import { PostsComponent } from './pages/posts/posts.component';
 import { SharedModule } from '../../shared/shared.module';
 import { CreateComponent } from './pages/create/create.component';
+import { AngularEditorModule } from '@kolkov/angular-editor';
 
 @NgModule({
   declarations: [PostsComponent, CreateComponent],
   imports: [
     CommonModule,
     PostsRoutingModule,
-    SharedModule
+    SharedModule,
+    AngularEditorModule
   ]
 })
 export class PostsModule { }
