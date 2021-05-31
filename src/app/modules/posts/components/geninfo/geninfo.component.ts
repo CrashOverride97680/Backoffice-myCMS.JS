8ef42a5a88a473b3edce9b0c890bf03dc99155d2
Fabrizio Manna <mannafabrizio83@gmail.com>
2021-05-31 12:33:06 +0200

    Modify geninfo.component.ts
    Implement form control elements

--- src/app/modules/posts/components/geninfo/geninfo.component.ts
+++ src/app/modules/posts/components/geninfo/geninfo.component.ts
@@ -1,8 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, forwardRef, Input} from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'app-geninfo',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => GeninfoComponent ),
+      multi: true
+    }
+  ],
   templateUrl: './geninfo.component.html',
   styleUrls: ['./geninfo.component.scss']
 })
-export class GeninfoComponent { }
+
+export class GeninfoComponent implements ControlValueAccessor {
+  constructor() {
+    this.onTouched = () => {};
+  }
+  onTouched: () => void;
+  @Input() lang = '-';
+  onChanged: any = () => {};
+
+  change($event: any) {
+    this.onTouched();
+    this.onChanged($event.target.value);
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChanged = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  writeValue(lang: string): void {
+    this.lang = lang;
+  }
+
+}
